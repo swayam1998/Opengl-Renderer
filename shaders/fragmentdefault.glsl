660b6b7dc981e147694d5c6c71663aa8b0f503af
swayam1998 <swayam1998@gmail.com>
2020-04-11 21:28:58 +0530

    implemented blinn-phong lighting

--- shaders/fragmentdefault.glsl
+++ shaders/fragmentdefault.glsl
@@ -26,13 +26,22 @@ out vec4 outColor;
 
 vec3 blinnPhongLighting (in vec3 kd, in vec3 ks, in float ns, in vec3 normal, in vec3 lightVector, in vec3 halfVector){
     // Fonction à écrire par les étudiants
-	return vec3(0.0, 0.0, 0.0);
+    
+    float cosTheta = abs(dot(normal, lightVector));
+    float cosAlpha = abs(dot(normal, halfVector));
+    vec3 colorFactor = (kd + ks*pow(cosAlpha, ns) * cosTheta);
+    
+	return colorFactor;
 }
 
 void main(void) {
 
     vec3 fragColor = vec3(0.0, 0.0, 0.0);
 	// Calculer fragColor;
+
+    fragColor = keyLightColor  * blinnPhongLighting(materialKd, materialKs, materialNs, varNormal, lightDirInView[0], halfVecInView[0]) + 
+                fillLightColor * blinnPhongLighting(materialKd, materialKs, materialNs, varNormal, lightDirInView[1], halfVecInView[1]) +
+                backLightColor * blinnPhongLighting(materialKd, materialKs, materialNs, varNormal, lightDirInView[2], halfVecInView[2]); 
 
     outColor = vec4( fragColor*globalColorFactor, 1.);
 
--- shaders/vertexdefault.glsl
+++ shaders/vertexdefault.glsl
@@ -32,14 +32,25 @@ out vec3 lightDirInView[3];
 out vec3 halfVecInView[3];
 
 
-void computeLightingVectorsInView(in vec3 posInView, in vec3 lightPosition, out vec3 lightDir, out vec3 halfVec){
-// Fonction à écrire par les étudiants
+void computeLightingVectorsInView(in vec3 posInView, in vec3 lightPosition, out vec3 lightDir, out vec3 halfVec)
+{
+    // Fonction à écrire par les étudiants
+
+    lightDir = normalize(lightPosition - posInView);
+    vec3 viewDir  = normalize(vec3(0.0f, 0.0f, 0.0f) - posInView);
+    halfVec = normalize(viewDir + lightDir);
+
 }
 
 void main(void) {
 
 // Calculez ici les informations géométriques (lightDirInView et halfVecInView) pour les sources de lumière
 
+vec3 posInView = vec3( modelViewMatrix * vec4(inPosition.xyz, 1.0));
+
+computeLightingVectorsInView(posInView, keyLightPosition , lightDirInView[0], halfVecInView[0]);
+computeLightingVectorsInView(posInView, fillLightPosition, lightDirInView[1], halfVecInView[1]);
+computeLightingVectorsInView(posInView, backLightPosition, lightDirInView[2], halfVecInView[2]);
 
 // Calcul du sommet
     varNormal = normalize((normalMatrix * vec4(inNormal,0.0)).xyz);
